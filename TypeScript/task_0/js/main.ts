980c168b046d59adee733c2095b81cdb91228b5c
Karolart <karoljuzz1610@gmail.com>
2023-04-02 23:21:10 -0500

    task0

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -1,9 +1,9 @@
 interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
-  }
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
   const student1: Student = {
       firstName: 'Bartolomeo',
       lastName: 'Simpson',
